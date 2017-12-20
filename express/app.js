3d3ab109d8357b9710f9ceed8f0ae473c63bfb48
Tyler Moon <moon.tyler@gmail.com>
2017-12-19 22:25:04 -0500

    Cleaned out unused packages

--- express/app.js
+++ express/app.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var creator = require('express-restful-api');
 
 var app = express();
 
+/**
+ * Express Configuration
+ */
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'ejs')
 
+/**
+ * Serve Angular 4 frontend
+ */
 // Angular DIST output folder
 app.use(express.static('../angular/dist'));
 
@@ -21,5 +24,6 @@ app.use(express.static('../angular/dist'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../angular/dist/index.html'));
 });
+
 
 module.exports = app;
